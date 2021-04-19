bdb178e532bb16fd2e3e36c99f91f4c31ae002d1
Cristianmen <32776012+Cristianmen@users.noreply.github.com>
2021-04-18 22:41:05 -0500

    ajuste ene eventos

--- src/app/pages/event/event.component.ts
+++ src/app/pages/event/event.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { HttpServiceService } from 'src/app/services/httpService/http-service.service';
 import { environment } from 'src/environments/environment';
 
@@ -8,17 +9,26 @@ import { environment } from 'src/environments/environment';
   styleUrls: ['./event.component.scss']
 })
 export class EventComponent implements OnInit {
+  formForo: FormGroup;
   
   isAlert = false;
   loading = false;
   configAlert = {};
 
-  dataRepos = [
+  dataEvents = [
     {
       eventId: '',
       nombre: '',
       fecha: '',
       hora:'',
+      tema: '',
+      descripcion:''
+    }
+  ];
+
+  dataForo = [
+    {
+      foroId: '',
       tema: '',
       descripcion:''
     }
@@ -39,6 +49,41 @@ export class EventComponent implements OnInit {
 
   ];
 
+  temas = [
+    "administración de empresa",
+    "universitaria arquitectura",
+    "biologia",
+    "biotecnologia",
+    "ciencias-politicas",
+    "comercio internacional",
+    "universitaria comunicación social",
+    "contaduria",
+    "universitaria derecho",
+    "universitaria diseño de modas",
+    "universitaria diseño gráfico",
+    "universitaria diseño industrial",
+    "universitaria economía",
+    "educacion-fisica",
+    "universitaria enfermería",
+    "universitaria finanzas",
+    "universitaria gastronomía",
+    "universitaria ingeniería ambiental",
+    "universitaria ingeniería civil",
+    "universitaria ingeniería de sistemas",
+    "universitaria ingeniería industrial",
+    "universitaria ingeniería mecánica",
+    "universitaria licenciatura en idiomas y lengua extranjera",
+    "universitaria matemáticas",
+    "universitaria medicina",
+    "universitaria medicina veterinaria",
+    "universitaria mercadeo y publicidad",
+    "universitaria música",
+    "universitaria odontología",
+    "universitaria optometria",
+    "universitaria psicología"
+   
+  ];
+
   configAlertTable = {
     type: 'warning',
     strong: 'Lo siento!',
@@ -48,7 +93,21 @@ export class EventComponent implements OnInit {
 
   constructor(
     private readonly serviceHttp: HttpServiceService,
-  ) { }
+    private readonly fb: FormBuilder,
+  ) {
+
+
+    this.formForo = this.fb.group({
+    
+      tema: new FormControl('', [
+        Validators.required
+      ]),
+      descripcion: new FormControl('', [
+        Validators.required
+      ])
+
+    });
+   }
 
   ngOnInit(): void {
     this.viewEventos();
@@ -63,9 +122,9 @@ export class EventComponent implements OnInit {
         this.loading = false;
 
         if (reponse.body.users.length > 0) {
-          this.dataRepos = reponse.body.users
+          this.dataEvents = reponse.body.users
         } else {
-          this.dataRepos = []
+          this.dataEvents = []
           this.isAlert = true;
           this.configAlert = this.configAlertTable
         }
@@ -80,6 +139,8 @@ export class EventComponent implements OnInit {
       })
 
   }
+
+  eliminar(i: any){}
   viewForo(){
     
     this.loading = true;
@@ -89,9 +150,9 @@ export class EventComponent implements OnInit {
         this.loading = false;
 
         if (reponse.body.users.length > 0) {
-          this.dataRepos = reponse.body.users
+          this.dataForo = reponse.body.users
         } else {
-          this.dataRepos = []
+          this.dataForo = [];
           this.isAlert = true;
           this.configAlert = this.configAlertTable
         }
@@ -105,5 +166,46 @@ export class EventComponent implements OnInit {
 
       })
   }
+
+  enviarForo(){
+
+    this.loading = true;
+    this.isAlert = false;
+    const date = new Date();
+
+    const body = {
+      foroId:`${Date.parse(date.toString())}`, 
+      tema: 'Prueba',
+      descripcion:this.getdescripcion()?.value
+    }
+    this.serviceHttp.requestHttp('post', `${environment.API}foro`, body).subscribe(
+      reponse => {
+        this.loading = false;
+        this.dataForo = []
+        this.viewForo()
+       
+        console.log('reponse', reponse);
+       
+
+      },
+      error => {
+        this.loading = false;
+      
+        console.log('error', error);
+
+      })
+
+
+
+
+  }
+  gettema() {
+    return this.formForo.get('tema');
+  }
+
+  getdescripcion() {
+    return this.formForo.get('descripcion');
+  }
+
 
 }
